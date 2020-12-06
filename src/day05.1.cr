faa14ac3213bde3403dc0266dc9258fad87cefbc
Romain Franceschini <franceschini.romain@gmail.com>
2020-12-06 11:32:14 +0100

    simplify day 5 using tr instead of regex + gsub

--- src/day05.1.cr
+++ src/day05.1.cr
@@ -1,8 +1,4 @@
 puts File
   .read_lines("#{__DIR__}/../input/05.dat")
-  .map { |n|
-    match = n.match(/^([FB]{7})([LR]{3})$/).not_nil!
-    row = match[1].gsub('F', "0").gsub('B', "1").to_i(2)
-    col = match[2].gsub('R', '1').gsub('L', '0').to_i(2)
-    row * 8 + col
-  }.max
+  .map(&.tr("FBRL", "0110").to_i(2))
+  .max
