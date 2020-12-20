ec40890871260adbe0df8ee362f051c34f2fc703
Romain Franceschini <franceschini.romain@gmail.com>
2020-12-20 21:50:09 +0100

    day 17

---
+++ src/day17.1.cr
@@ -0,0 +1,56 @@
+record Point, x : Int32, y : Int32, z : Int32 do
+  include Comparable(Point)
+
+  def <=>(other : Point)
+    {@x, @y, @z} <=> {other.x, other.y, other.z}
+  end
+
+  def -(amount : Int)
+    Point.new(@x - amount, @y - amount, @z - amount)
+  end
+
+  def +(amount : Int)
+    Point.new(@x + amount, @y + amount, @z + amount)
+  end
+
+  def upto(other : Point)
+    @x.upto(other.x) { |x|
+      @y.upto(other.y) { |y|
+        @z.upto(other.z) { |z|
+          yield Point.new(x, y, z)
+        }
+      }
+    }
+  end
+
+  def each_neighbor
+    (self - 1).upto(self + 1) { |p| yield p if p != self }
+  end
+end
+
+env = Hash(Point, Bool).new { |h, k| h[k] = false }.tap { |env|
+  File.read_lines("#{__DIR__}/../input/17.dat").each_with_index { |row, y|
+    row.chars.each_with_index { |char, x|
+      env[Point.new(x, y, 0)] = char == '#'
+    }
+  }
+}
+
+def count_active_neighbors(point, env)
+  count = 0
+  point.each_neighbor { |neighbor| count += 1 if env[neighbor] }
+  count
+end
+
+6.times {
+  env = env.dup.tap { |next_env|
+    min, max = env.each_key.minmax
+    (min - 1).upto(max + 1) { |p|
+      nalive = count_active_neighbors(p, env)
+      next_env[p] = (env[p] && (nalive == 2 || nalive == 3)) ||
+                    (!env[p] && nalive == 3)
+    }
+  }
+}
+
+puts env.each_value.count(true)
